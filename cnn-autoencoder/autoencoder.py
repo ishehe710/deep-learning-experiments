8915a5a9e737371c38139403f4acf7d2825db43b
ishehe710 <ishehe@arizona.edu>
2026-03-14 00:08:52 -0700

    finished the autoencoder experiments

---
+++ cnn-autoencoder/autoencoder.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
+
+class AutoEncoder(nn.Module):
+    def __init__(self, latent_dim=32):    
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(16, 32, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(in_features=32 * 4 * 4, out_features=latent_dim)
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(in_features=latent_dim, out_features=32 * 4 * 4),
+            nn.Unflatten(1, (32, 4, 4)),
+            nn.ConvTranspose2d(32, 16, kernel_size=5, stride=2, padding=0, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, kernel_size=5, stride=2, padding=0, output_padding=1),
+            nn.Sigmoid()        
+        )
+
+    
+    def forward(self, x):
+
+        z = self.encoder(x)
+
+        reconstruction = self.decoder(z)
+
+        return reconstruction
+    
+    def training_step(self, batch):
+        """Returns the loss for a batch of training data"""
+        images, _ = batch
+        out = self(images)                  # Generate predictions
+        loss = F.mse_loss(out, images) # Calculate loss
+        return loss
+        
+    def epoch_end(self, outputs):
+        batch_losses = outputs
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        return {'loss': epoch_loss.item()}
+
+    def fit(self, train_loader, val_loader=None, epochs=10, lr=0.001):
+
+        history = []
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+
+        for epoch in range(epochs):
+
+            self.train()  # set model to training mode
+            batch_losses = []
+
+            for batch in train_loader:
+
+                loss = self.training_step(batch)
+
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad(set_to_none=True)
+
+                batch_losses.append(loss.detach())
+
+            train_loss = torch.stack(batch_losses).mean().item()
+
+            if val_loader:
+                val_result = self.evaluate(val_loader)
+                print(f"Epoch [{epoch+1}/{epochs}] train_loss: {train_loss:.4f}, val_loss: {val_result['loss']:.4f}")
+                history.append((train_loss, val_result['loss']))
+            else:
+                print(f"Epoch [{epoch+1}/{epochs}] train_loss: {train_loss:.4f}")
+                history.append(train_loss)
+
+        return history
+    
+    def evaluate(self, loader):
+
+        self.eval()  # set evaluation mode
+
+        with torch.no_grad():
+            outputs = [self.training_step(batch) for batch in loader]
+
+        return self.epoch_end(outputs)
+    
+    # one image
+    def visualize_result(self, output, original):
+        # Create a 1x2 layout
+        fig, axs = plt.subplots(1, 2, figsize=(8, 4))
+        axs[0].imshow(output.detach().cpu().squeeze().numpy(), cmap='gray')
+        axs[0].set_title("Generated Image")
+        axs[0].axis('off')  
+        
+        axs[1].imshow(original.detach().cpu().squeeze().numpy(), cmap='gray')
+        axs[1].set_title("Original Image")
+        axs[1].axis('off')  
+        
+        plt.tight_layout()
+        plt.show()
+        
